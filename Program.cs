c8ce6373e8a2e812940c3b22a60f4b194a9be726
Aziz <azizka.ibragimov@gmail.com>
2021-01-19 14:14:55 +0200

    first fiqnya

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace vahav4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int chislo = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(chislo % 10 * 100 + chislo / 10);
+
+        
+        }
+    }
+}
